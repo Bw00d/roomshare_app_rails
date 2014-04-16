a3ee4826376402c10779ffe92393601e43e04d6a
Brentwood Reid & Corey Norris <student@epicodus.com>
2014-04-16 09:26:49 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+#roomshare_app
+Rails 4.0.4
+pairs Corey Norris & Brentwood Reid
+###Epicodus exercise
+
+This is an room share application styled of airBandB or couchsurfer. The purpose of this exercise is to become more proficient with Rails.  We will be utilizing Devise for user authorization and Rspec, Capybara and FactoryGirl for testing. We will practice ActionMailer and will implement ajax where practical for UX.
+
+* The user will be able to create an account
+* The user will receive a confirmation email
+* The user will be able to post an accomodation
+* The user will be able to browse accomodations by location
+* The user will be able to book and accomodation
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RoomshareApp::Application.config.secret_key_base = '8f5f468ce7fee30b5091b6bfeddf560be577c006098a55068957379b40937b2a63d545fc7537d0cf8befe610bb9683d00eaa37537e21af6905be29643d8b3f51'
