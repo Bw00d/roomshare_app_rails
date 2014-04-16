871acd8a2339a59f30e0c0db7769824153398e3b
Brentwood Reid & Corey Norris <student@epicodus.com>
2014-04-16 16:50:30 -0700

    add routes, new, and show views for accomidation model

--- app/controllers/accomodations_controller.rb
+++ app/controllers/accomodations_controller.rb
@@ -1,5 +1,27 @@
 class AccomodationsController < ApplicationController
 
+  def index
+
+  end
+
+  def show
+    @accomodation = Accomodation.find(params[:id])
+  end
+
+  def new
+    @accomodation = Accomodation.new
+  end
+
+  def create
+    @user = current_user
+    @accomodation = Accomodation.create(accomodations_params)
+    if @accomodation.save
+      @user.accomodations << @accomodation
+      redirect_to user_accomodations_path, :notice => "#{@product.name} added to your store"
+    else
+      render new_accomodation_path
+    end
+  end
 
 private
     def accomodations_params
--- config/accomodations.rb
+++ config/accomodations.rb
@@ -2,8 +2,7 @@ RoomshareApp::Application.routes.draw do
   root to: 'users#home'
 
   devise_for :users, path_names: { sign_in: "login", sign_out: "logout" }
-  resources :users, only: [:show] do
-    resources :accomodations
-  end
+
   resources :accomodations
+  resources :users, only: [:show]
 end
