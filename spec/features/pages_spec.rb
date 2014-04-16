4fa858cf604f73a1a2276a19041bf45944cb6ae5
Brentwood Reid & Corey Norris <student@epicodus.com>
2014-04-16 12:03:49 -0700

    add passing specs for homepage links and titles

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -1,7 +1,27 @@
 require 'spec_helper'
 
-describe 'header' do
+feature 'homepage' do
   before { visit root_url }
-  it 'should have "Login" link' do
-    expect(page).to have_link "Login"
+
+  it 'should have appropriate title' do
+    expect(page).to have_title "RoomShare"
+  end
+
+  describe 'header' do
+    it '"Login" link should take you to the proper page' do
+      click_link 'Login'
+      expect(page).to have_title "Login | RoomShare"
+    end
+
+    it '"Sign up" link should take you to the proper page' do
+      click_link 'Sign up'
+      expect(page).to have_title "Sign up | RoomShare"
+    end
+
+    it 'should have a "RoomShare" link' do
+      click_link 'RoomShare'
+      expect(page).to have_title "RoomShare"
+    end
+  end
 end
+
