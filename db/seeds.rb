0c4d9eca4cd24eac6ed2c528fdc24b0c72a70d53
Corey Norris & Brentwood Reid <student@epicodus.com>
2014-04-17 16:06:13 -0700

    experiment with seeding database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+
+def make_ny
+    ny = FactoryGirl.create(:accomodation, location: "New York")
+    @user.accomodations << ny
+end
+  def make_paris
+      paris = FactoryGirl.create(:accomodation, location: "Paris")
+      @user.accomodations << paris
+end
+def make_tokyo
+    tokyo = FactoryGirl.create(:accomodation, location: "Tokyo")
+    @user.accomodations << tokyo
+end
+
+
+
+  10.times do |n|
+    email = Faker::Internet.safe_email
+    password = "foobarbaz"
+    @user = FactoryGirl.create(:user, email: email, password: password,
+                 password_confirmation: password)
+    make_ny
+    make_paris
+    make_tokyo
+  end
+
